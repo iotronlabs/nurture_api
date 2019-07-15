c447be350e0be6cdc035bf339832345a6a1a8cc0
Axle10 <saptarshi.sinha.ind@gmail.com>
2019-07-16 00:08:08 +0530

    db updated

--- database/migrations/2019_07_12_172133_create_user_admins_table.php
+++ database/migrations/2019_07_12_172133_create_user_admins_table.php
@@ -18,6 +18,7 @@ class CreateUserAdminsTable extends Migration
             $table->string('name');
             $table->string('email');
             $table->string('password');
+            $table->integer('authentication')->default('5')->length(1);
             $table->timestamps();
         });
     }
